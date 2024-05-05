c83fdf9060d697be971ccdadfff6cd8c4eb7755f
Vishal Khulape <142664838+vishalkhulape123@users.noreply.github.com>
2024-05-05 01:23:57 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,17 @@
+"use srict"; // trits all JS code as new version
+
+//alert(100 + 100); // we are using nodeJS not browser as this is not defined in nodeJS
+
+//number => 2 pow 53
+//bigint
+//string 
+// boolean => true/false
+//undefined => 
+//null => standalone value
+//symbol => unique
+//object => 
+    
+
+    // interview question
+    console.log(typeof undefined); // undefined
+    console.log(typeof null );  // object
