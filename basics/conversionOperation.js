79d591717d6b4e223be513a9ba277a14f71227ed
Vishal Khulape <142664838+vishalkhulape123@users.noreply.github.com>
2024-05-05 01:38:42 +0000

    conversionOperation

---
+++ basics/conversionOperation.js
@@ -0,0 +1,24 @@
+let score = "55";
+
+let value = Number(score);
+console.log(typeof score);
+/*
+  "55" = string
+  true/false = 1/0
+  undefined = NaN
+  null = 0
+*/
+
+let isCorrect = ""
+
+let bValue = Boolean(isCorrect)
+
+/*
+  true => true
+  "" => false
+  "Vishal" => true
+  1 => true
+  0 => false
+*/
+
+console.log(bValue);
